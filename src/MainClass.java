93470a211c674bf5a6eb5b7397d64f2ebd3dd124
Guillaume Perouffe <g.perouffe@gmail.com>
2015-04-28 14:02:34 +0200

    Revert "Nouvelle résolution mini fixes #5"

--- src/MainClass.java
+++ src/MainClass.java
@@ -8,6 +8,7 @@ import fr.fablabmars.vue.Fenetre;
 public class MainClass {
 
 	public static void main(String[] args) {
+		// Chargement du driver JDBC pour MySQL
 		try {
 		    Class.forName( "com.mysql.jdbc.Driver" );
 		} catch ( ClassNotFoundException e ) {
